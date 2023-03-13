f6b2e7e961ed46c52b5eda5ee86e0a454a0d4955
datracka <datracka@gmail.com>
2023-03-13 17:36:05 +0100

    add all

---
+++ src/main/java/com/oman/oman/OmanApplication.java
@@ -0,0 +1,13 @@
+package com.oman.oman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmanApplication.class, args);
+	}
+
+}
