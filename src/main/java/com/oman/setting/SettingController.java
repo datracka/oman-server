5b82599bfca6bf6bb720e413e43fa65d4c345a69
datracka <datracka@gmail.com>
2023-03-30 17:51:36 +0200

    add dummy controller

--- src/main/java/com/oman/setting/SettingController.java
+++ src/main/java/com/oman/setting/SettingController.java
@@ -1,16 +1,22 @@
 package com.oman.setting;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.oman.todoitem.TodoItem;
 
 // import com.oman.todoitem.TodoItem;
 
 @RestController
+@RequestMapping("setting")
 public class SettingController {
-    /*
-     * @GetMapping("/setting/{id}")
-     * public ResponseEntity<TodoItem> findById(@PathVariable Long id) {
-     * return null;
-     * }
-     */
+
+    @GetMapping
+    public ResponseEntity<TodoItem> getSeettings() {
+        return ResponseEntity.ok(new TodoItem());
+    }
 
 }
